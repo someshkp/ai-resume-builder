7986eae4501c7801f04bb5016f45ac872366b972
Somesh <somesh.patnaik@purpleplumfi.com>
2026-06-15 14:40:07 +0530

    white lisiting the backend url

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.BASE_URL || "http://localhost:5173",
     credentials: true,
   }),
 );
