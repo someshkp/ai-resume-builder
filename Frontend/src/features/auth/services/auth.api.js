bc62e2f349d30b79c5553b8a9b1686ded205b2e0
Somesh <somesh.patnaik@purpleplumfi.com>
2026-06-15 14:27:42 +0530

    added base url

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/auth",
+  baseURL: process.env.BASE_URL || "http://localhost:3000/api/auth",
   withCredentials: true,
 });
 
